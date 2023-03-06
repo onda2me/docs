547ee6ad3ddcee252b755225a704858a67c8fc58
onda2me <onda2me@naver.com>
2023-03-06 15:34:50 +0900

    add cospro

---
+++ startup.bat
@@ -0,0 +1,16 @@
+@echo off
+rem ----------------------------
+rem :: 사용방법
+rem :: cd [서비스 디렉토리 경로]
+rem :: jekyll serve (bundle exec jekyll serve)
+rem ----------------------------
+
+rem cd C:\Users\Administrator\Documents\GitHub\onda2me\
+rem jekyll serve
+
+@echo on
+rem ----------------------------
+rem Jekyll Server Start!!
+rem ----------------------------
+bundle exec jekyll serve
+
